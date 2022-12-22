f412d25afdde1c335463183d7a01708833a4c0aa
Arjaz <arjaz@protonmail.com>
2022-12-22 13:32:38 +0200

    Implement immediate constants compilation

--- compiler.scm
+++ compiler.scm
@@ -2,14 +2,54 @@
 (define (emit instruction . args)
   (set! program (append program (list (apply format #f (string-append instruction "~%") args)))))
 
+;; fixnums, booleans, characters and ()
+;; immediate constant representation:
+;; tag + content
+;; fixnum tag: 0b00 in the two least significant bits, 30 bits of data
+;; other constants: 0b1111 in the two least significatnt bits
+;; #f: 0b00101111 - 2 bytes
+;; #t: 0b01101111 - 2 bytes
+;; character: #b00001111 tag, one byte to represent the value
+;; (): #b00111111 - 2 bytes
+
+(define fixnum-shift 2)
+(define fixnum-mask #b11)
+(define bool-t #b00101111)
+(define bool-f #b01101111)
+(define char-shift 8)
+(define char-mask #b00001111)
+(define empty-list #b00111111)
+(define wordsize 4) ; bytes
+
+(define fixnum-bits (- (* wordsize 8) fixnum-shift)) ; 30 bits
+
+(define fixnum-min (- (expt 2 (- fixnum-bits 1))))
+(define fixnum-max (- (expt 2 (- fixnum-bits 1)) 1))
+
+(define (fixnum? x)
+  (and (integer? x) (exact? x) (<= fixnum-min x fixnum-max)))
+
+(define (immediate? x)
+  (or (fixnum? x) (boolean? x) (char? x) (null? x)))
+
+(define (immediate-rep x)
+  (cond
+   [(fixnum? x) (ash x fixnum-shift)]
+   [(eq? x #t)  bool-t]
+   [(eq? x #f)  bool-f]
+   [(char? x)   (logior (ash (char->integer x) char-shift) char-mask)]
+   [(null? x)   empty-list]
+   [else        (error "not an immediate constant")]))
+
 (define (compile-program x)
-  (unless (integer? x) (error "only fixnum integers are supported"))
+  (unless (immediate? x) (error "only immediate constant are supported"))
   (set! program '())
   (emit "    .text")
   (emit "    .globl scheme_entry")
   (emit "    .type scheme_entry, @function")
   (emit "scheme_entry:")
-  (emit "    movl $~a, %eax" x)
+  (emit "    # ~s, binary: 0b~a" x (number->string (immediate-rep x) 2))
+  (emit "    movl $~a, %eax" (immediate-rep x))
   (emit "    ret")
   program)
 
@@ -20,4 +60,4 @@
         (display asm-source port)
         (close-port port)))))
 
-(write-program "target/scheme.s" (compile-program 18))
+(write-program "target/scheme.s" (compile-program '()))
--- runtime.c
+++ runtime.c
@@ -1,6 +1,35 @@
 #include <stdio.h>
 
+#define fixnum_shift 2
+#define fixnum_mask 0b11
+#define fixnum_tag 0b00
+#define char_shift 8
+#define char_mask 0b11111111
+#define char_tag 0b00001111
+#define	bool_t 0b00101111
+#define	bool_f 0b01101111
+#define empty_list 0b00111111
+
+typedef unsigned int scm_ptr;
+
+static void print_scm_ptr(scm_ptr x) {
+	if ((x & fixnum_mask) == fixnum_tag) {
+		printf("%d", ((int) x) >> fixnum_shift);
+	} else if ((x & char_mask) == char_tag) {
+		printf("%c", ((int) x) >> char_shift);
+	} else if (x == bool_f) {
+		printf("#f");
+	} else if (x == bool_t) {
+		printf("#t");
+	} else if (x == empty_list) {
+		printf("()");
+	} else {
+		printf("#<unknown 0x%08x>", x);
+	}
+	printf("\n");
+}
+
 int main(int argc, char** argv) {
-	printf("%d\n", scheme_entry());
+	print_scm_ptr(scheme_entry());
 	return 0;
 }
